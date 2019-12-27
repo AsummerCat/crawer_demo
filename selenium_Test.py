8be375e98dd1240a51876b707e0af90d85953378
chenxiaocehng <583188551@qq.com>
2019-12-27 17:30:56 +0800

    selenium 将chromedriver自动化工具放入根目录

---
+++ selenium_Test.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+'''
+chromedriver
+http://npm.taobao.org/mirrors/chromedriver/
+
+ conda install -c conda-forge selenium
+
+'''
+from selenium import webdriver
+
+
+def test():
+    driver = webdriver.Chrome()  # 创建一个 Chrome WebDriver 实例
+    driver.get('https://www.baidu.com/')  # 打开网址
+
+if __name__ == '__main__':
+    test()
